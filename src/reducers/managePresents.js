24ca69886d3de168519601b516c528bc684afdcd
jathmel <jathmel@gmail.com>
2018-08-02 11:45:20 -0400

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,14 @@
+let action = {
+  type: "INCREASE"
+}
+
 export function managePresents(state, action){
+  switch (action.type) {
+    case "INCREASE":
+        return {numberOfPresents: state.numberOfPresents + 1}
+      break;
+    default:
+      return state
+
+  }
 }
