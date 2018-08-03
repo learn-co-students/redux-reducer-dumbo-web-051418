d3e29a6212e8702b89a2f655ef76d1fbdb7c667d
prmoreira23 <prm.gredes@gmail.com>
2018-08-03 14:46:51 -0400

    Done with var.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+  switch(action.type){
+    case "ADD_FRIEND":
+      // I used 'var', screw you ES6. @matt-mcalister.
+      var newArray = [...state.friends, action.friend];
+      return {...state, friends: newArray};
+    case "REMOVE_FRIEND":
+      var newArray = state.friends.filter(friend => friend.id !== action.id);
+      return {...state, friends: newArray};
+    default:
+       return state;
+  }
 }
